51e4c02a238589b050ed7b795b587f5d959635d8
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-01-15 00:25:36 +0300

    association

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,29 @@ height:u32
  }
 
 fn main() {
-    let r1:Rectangle=Rectangle { width:50000, height: 80000};
-    println!("area of rectangle R1={}",r1.area());
+    // let r1:Rectangle=Rectangle { width:50000, height: 80000};
+    // println!("area of rectangle R1={}",r1.area());
+    let light:TrafficLight=TrafficLight::new();
+
+    assert_eq!(light.color,"red");
+    assert_eq!(light.get_state(),"red");
+    println!("success")
+
+}
+// impliment associationm. they dont take self
+struct TrafficLight{
+    color:String
+
+}
+impl TrafficLight{
+    pub fn new()->Self{
+        Self{
+            color:"red".to_string()
+        }
+    
+    }
+    
+    pub fn get_state(&self)->&str{
+        &self.color
+    }
 }
