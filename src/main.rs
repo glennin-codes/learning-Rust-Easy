bc7332899d48ca6bfa3510ace7811ca569235971
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-01-14 23:58:45 +0300

    implimented methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Rectangle{
+width:u32,
+height:u32
+}
+ impl Rectangle{
+    fn area(&self)->u32{
+          self.width* self.height
+    }
+ }
+
 fn main() {
-    println!("Hello, world!");
+    let r1:Rectangle=Rectangle { width:50000, height: 80000};
+    println!("area of rectangle R1={}",r1.area());
 }
