7630ab90a90d506ebac7364dbe4f58e4de00404f
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-01-15 22:42:00 +0300

    enums /impl and associations

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,27 @@
-struct Rectangle{
-width:u32,
-height:u32
-}
- impl Rectangle{
-    fn area(&self)->u32{
-          self.width* self.height
-    }
- }
-
-fn main() {
-    // let r1:Rectangle=Rectangle { width:50000, height: 80000};
-    // println!("area of rectangle R1={}",r1.area());
-    let light:TrafficLight=TrafficLight::new();
-
-    assert_eq!(light.color,"red");
-    assert_eq!(light.get_state(),"red");
-    println!("success")
-
-}
-// impliment associationm. they dont take self
-struct TrafficLight{
-    color:String
-
+// 
+//implementing enums
+#[derive(Debug)]
+enum TrafficLight{
+    Yellow,
+    Red,
+    Green,
 }
 impl TrafficLight{
-    pub fn new()->Self{
-        Self{
-            color:"red".to_string()
-        }
-    
-    }
-    
-    pub fn get_state(&self)->&str{
-        &self.color
-    }
+  fn color(&self)-> &str{
+      match self{
+        Self::Yellow=>"yellow",
+        Self::Red => "red",
+        Self::Green => "green",
+        
+        
+      }
+  }
+}
+fn main(){
+    let wel: &str="hello world..";
+    let valid_string="shjjhsd ".to_string()+wel + &"djksd".to_string();
+    println!("testing smth:{}",valid_string);
+    let c :TrafficLight=TrafficLight::Yellow;
+    println!("color of enum Yellow is {}",c.color());
+    println!("succes {:?}",c)
 }
