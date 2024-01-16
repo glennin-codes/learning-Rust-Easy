c6266807a2ecbea84857147cff9a6bfea48f88ab
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-01-16 12:47:42 +0300

    tupples

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-fn main(){
-  let mut years:[i32;3]=[2001,2005,2010];
-  years[0]=2020;
+// fn main(){
+//   let mut years:[i32;3]=[2001,2005,2010];
+//   years[0]=2020;
 
-  // for year in years.iter(){
-  //   println!("year:{}",year + year);
-  // }
-let [x,..]=years;
-println!("value is{}",x);
+//   // for year in years.iter(){
+//   //   println!("year:{}",year + year);
+//   // }
+// let [x,..]=years;
+// println!("value is{}",x);
+// }
+//tuples
+fn main(){
+  let foo:(u32,bool,char)=(2882,true,'G');
+  let (_x,_y,..)=foo;
+  println!("my tuple is {:?}",foo);
+
 }
