6d2888f2540dccce14556e71061b30fc5f52a927
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-01-16 10:03:17 +0300

    task from frontend master

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-// 
-//implementing enums
-#[derive(Debug)]
-enum TrafficLight{
-    Yellow,
-    Red,
-    Green,
+fn main() {
+    let city_name = "Rustville";
+
+    println!("The city of {}:\n", city_name);
+
+    print_population(1_324_578, 114_293, 108_097);
 }
-impl TrafficLight{
-  fn color(&self)-> &str{
-      match self{
-        Self::Yellow=>"yellow",
-        Self::Red => "red",
-        Self::Green => "green",
-        
-        
-      }
-  }
-}
-fn main(){
-    let wel: &str="hello world..";
-    let valid_string="shjjhsd ".to_string()+wel + &"djksd".to_string();
-    println!("testing smth:{}",valid_string);
-    let c :TrafficLight=TrafficLight::Yellow;
-    println!("color of enum Yellow is {}",c.color());
-    println!("succes {:?}",c)
+
+fn print_population(adults: u64, kids: u32, buildings: u32) {
+    // 👉 TODO compute population by adding adults to kids
+    //
+    // 💡 TIP: Use the `as` keyword to convert between numeric types!
+    let population = adults as u32 + kids as u32;
+      
+    // 👉 TODO compute buildings_per_person by dividing buildings by population
+    //
+    // 💡 TIP: To get a f64 answer here, both numerator and denominator must be f64 values
+    let buildings_per_person = (buildings /population) as f64;
+
+    println!("    Population: {}", population);
+    println!("        Adults: {}", adults);
+    println!("        Kids: {}", kids);
+    println!("    Buildings: {}", buildings);
+    println!("    Buildings per person: {}\n",buildings_per_person);
+
+    if buildings_per_person >= 1.0 {
+        println!("Everyone can have their own building!");
+    } else {
+        println!("Buildings must be shared!");
+    }
 }
