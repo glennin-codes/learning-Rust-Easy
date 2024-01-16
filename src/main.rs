73f54374a3d86bdddffb67377e61046d46b7d1e6
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-01-16 11:02:13 +0300

    revisting arrays

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,8 @@
-fn main() {
-    let city_name = "Rustville";
+fn main(){
+  let years:[i32;3]=[2001,2005,2010];
 
-    println!("The city of {}:\n", city_name);
+  for year in years.iter(){
+    println!("year:{}",year + year);
+  }
 
-    print_population(1_324_578, 114_293, 108_097);
-}
-
-fn print_population(adults: u64, kids: u32, buildings: u32) {
-    // 👉 TODO compute population by adding adults to kids
-    //
-    // 💡 TIP: Use the `as` keyword to convert between numeric types!
-    let population = adults as u32 + kids as u32;
-      
-    // 👉 TODO compute buildings_per_person by dividing buildings by population
-    //
-    // 💡 TIP: To get a f64 answer here, both numerator and denominator must be f64 values
-    let buildings_per_person = (buildings /population) as f64;
-
-    println!("    Population: {}", population);
-    println!("        Adults: {}", adults);
-    println!("        Kids: {}", kids);
-    println!("    Buildings: {}", buildings);
-    println!("    Buildings per person: {}\n",buildings_per_person);
-
-    if buildings_per_person >= 1.0 {
-        println!("Everyone can have their own building!");
-    } else {
-        println!("Buildings must be shared!");
-    }
 }
