73fab04fa8675a6b042e07f64d8264ee2e17165d
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-01-16 13:11:31 +0300

    reviewing structs

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,33 @@
 // println!("value is{}",x);
 // }
 //tuples
-fn main(){
-  let foo:(u32,bool,char)=(2882,true,'G');
-  let (_x,_y,..)=foo;
-  println!("my tuple is {:?}",foo);
+// fn main(){
+//   let mut  foo:(u32,bool,char)=(2882,true,'G');
+// foo.0=900;
+//   println!("my tuple is {:?}",foo);
 
+// }
+//structs
+struct Student{
+  age:u16,
+  name:String,
+  id:String,
+}
+impl Student {
+  pub fn new_student(age:u16,name:String,id:String)->Self{
+    Self{
+      age,
+      name,
+      id
+    }
+  }
+  fn get_age(&self)->u16{
+    self.age
+  }
+    
+}
+fn main(){
+  
+let student_a = Student::new_student(21, "glen ayienda".to_string(), "janme3434".to_string());
+  println!("student age is {}",student_a.get_age());
 }
