efb0094d0ff9189569fa3c4fa2e281ad6f496631
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-03-24 23:49:03 +0300

    completed hashmaps

--- hash-map/README.md
+++ hash-map/README.md
@@ -40,5 +40,29 @@ for (key,value) in scores{
 }
 ```
 
+### use or_insert("value") or or_insert_with()
+- Ensures a value is in the entry by inserting the default if empty, and returns a mutable reference to the value in the entry.
+- ``or_insert() `` its a function that takes a concrete value as its parameter.
+- ``or_insert_with() ``  takes a closure that generates the default value.
 
+```rust
+let mut player_stats = HashMap::new();
+
+    // Insert a key only if it doesn't already exist
+    player_stats.entry("health").or_insert(100);
+
+
+```
+- In terms of usage, if you already know the default value you want to insert, or_insert() is simpler and more straightforward. However, if the default value requires computation or you want to ensure that it's lazily evaluated only when necessary, or_insert_with() is more appropriate because it allows you to pass a closure that calculates the default value.
+```rust 
+let mut player_stats = HashMap::new();
+
+    // Insert a key only if it doesn't already exist
+    player_stats.entry("health").or_insert_with(random_value);
+    fn random_value ->u8 (){
+        // Could actually return some random value here - let's just return
+    // some fixed value for now
+    26
+    }
+```
 
--- hash-map/src/main.rs
+++ hash-map/src/main.rs
@@ -1,40 +1,72 @@
-use::std::collections::HashMap;
-use std::ptr::null;
+// use::std::collections::HashMap;
+// use std::ptr::null;
 
 
 
-fn main(){
-    let mut scores: HashMap<&str, i32>=HashMap::new();
-    scores.insert("oranges", 3);
-    scores.insert("mangoes", 4);
-    scores.insert("guavas", 3);
+// fn main(){
+//     let mut scores: HashMap<&str, i32>=HashMap::new();
+//     scores.insert("oranges", 3);
+//     scores.insert("mangoes", 4);
+//     scores.insert("guavas", 3);
 
-    println!("{:?}",scores);
-    //removing a value 
-    scores.remove("oranges");
-    println!("After removing oranges : {:?}",scores);
-//getting a value
-let  get_value: Option<&i32>=scores.get("oranges");
-//using if let
-// if let Some(value)=get_value {
-//     println!("The value of mangoes is : {:?}",value);
-// }else {
-//     println!("The value of mangoes is not found");
+//     println!("{:?}",scores);
+//     //removing a value 
+//     scores.remove("oranges");
+//     println!("After removing oranges : {:?}",scores);
+// //getting a value
+// let  get_value: Option<&i32>=scores.get("oranges");
+// //using if let
+// // if let Some(value)=get_value {
+// //     println!("The value of mangoes is : {:?}",value);
+// // }else {
+// //     println!("The value of mangoes is not found");
+// // }
+
+// //using match
+
+// // match get_value{
+// //     Some(value)=>println!("The value of mangoes is : {:?}",value),
+// //     None=>println!("The value of mangoes is not found")
+// // }
+
+// //using unwrap
+// // - unwrap method is used to get the value of the key from the hashmap if your sure the value is present in the hashmap
+// // - if the value is not present in the hashmap it will panic
+// // println!("The value of mangoes is : {:?}",get_value.unwrap());
+// //iterating through the hashmap
+// for (key,value) in scores{
+//     println!("key:{} value:{}",key,value);
 // }
-
-//using match
-
-// match get_value{
-//     Some(value)=>println!("The value of mangoes is : {:?}",value),
-//     None=>println!("The value of mangoes is not found")
 // }
+// FILL in the blanks
+use std::collections::HashMap;
+fn main() {
+    // Type inference lets us omit an explicit type signature (which
+    // would be `HashMap<&str, u8>` in this example).
+    let mut player_stats = HashMap::new();
 
-//using unwrap
-// - unwrap method is used to get the value of the key from the hashmap if your sure the value is present in the hashmap
-// - if the value is not present in the hashmap it will panic
-// println!("The value of mangoes is : {:?}",get_value.unwrap());
-//iterating through the hashmap
-for (key,value) in scores{
-    println!("key:{} value:{}",key,value);
+    // Insert a key only if it doesn't already exist
+    player_stats.entry("health").or_insert(100);
+
+    assert_eq!(player_stats["health"], 100);
+
+    // Insert a key using a function that provides a new value only if it
+    // doesn't already exist
+    player_stats.entry("health").or_insert_with(random_stat_buff);
+    assert_eq!(player_stats["health"], 100);
+
+    // Ensures a value is in the entry by inserting the default if empty, and returns
+    // a mutable reference to the value in the entry.
+    let health = player_stats.entry("health").or_insert(50);
+    assert_eq!(*health, 100);
+    *health -= 50;
+    assert_eq!(*health, 50);
+
+    println!("Success!");
 }
+
+fn random_stat_buff() -> u8 {
+    // Could actually return some random value here - let's just return
+    // some fixed value for now
+    42
 }
